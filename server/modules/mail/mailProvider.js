5edf9b5d8f022009e042ba686372f0e8b29cc89a
Pawel Warmuth <warmuth.pawel@gmail.com>
2017-03-17 19:18:33 +0100

    Changed Mail: Now the Admin has to activate the User

--- server/modules/mail/mailProvider.js
+++ server/modules/mail/mailProvider.js
@@ -1,12 +1,34 @@
 var helper = require('sendgrid').mail;
 
-exports.SendMailToUser = (userMail, guid) => {
+/* exports.SendMailToUser = (userMail, guid) => {
     var from_email = new helper.Email('webmaster@black-mesa.de');
     var to_email = new helper.Email(userMail);
     var subject = 'Account Activation';
     var content = new helper.Content('text/plain',
         'Hi, please click the following URL to Activate your user : '
         + process.env.AllowedHost + '/unlock/' + guid);
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+    var sg = require('sendgrid')(process.env.SendGridKey);
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON(),
+    });
+
+    sg.API(request, function (error, response) {
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+    });
+
+} */
+
+exports.SendMailToUser = (user, guid) => {
+    var from_email = new helper.Email('webmaster@black-mesa.de');
+    var to_email = new helper.Email('webmaster@black-mesa.de');
+    var subject = 'Account Activation';
+    var content = new helper.Content('text/plain',
+        'A User has to be activated: ' + user.id);
     var mail = new helper.Mail(from_email, subject, to_email, content);
     var sg = require('sendgrid')(process.env.SendGridKey);
     var request = sg.emptyRequest({
